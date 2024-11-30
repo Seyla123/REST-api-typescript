80a3527428c6e61670137a801902b75c5a660858
Seav Seyla <mrrseyla.758@gmail.com>
2024-11-30 21:23:27 +0700

    feat(todo):add create todo

--- src/controller/todoController.ts
+++ src/controller/todoController.ts
@@ -1,46 +1,17 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import { Todo } from "../model";
+import { v4 as uuidv4 } from "uuid";
 
 // create todo
-export const createTodo = (req: Request, res:Response) => {
-    const { title } = req.body;
-    res.status(201).json({
-        message: 'Todo created successfully',	
-        title
-    });
-}
-
-// get all todos
-export const getAllTodos = (req: Request, res:Response) => {
-    res.status(200).json({
-        message: 'All todos fetched successfully',	
-    });
-}
-
-// update todo
-export const updateTodo = (req: Request, res:Response) => {
-    const { id } = req.params;
-    const { title, completed } = req.body;
-    res.status(200).json({
-        message: 'Todo updated successfully',	
-        title,
-        id,
-        completed
-    });
-}
-
-// delete todo
-export const deleteTodo = (req: Request, res:Response) => {
-    const { id } = req.params;
-    res.status(200).json({
-        message: 'Todo deleted successfully',	
-    });
-}
-
-// get todo by id
-export const getTodoById = (req: Request, res:Response) => {
-    const { id } = req.params;
-    res.status(200).json({
-        message: 'Todo fetched successfully',	
-        id
-    });
-}
+export const createTodo = async (req: Request, res: Response) => {
+  const { title } = req.body;
+  const todo = await Todo.create({
+    title: title,
+    id: uuidv4(),
+    completed: false,
+  });
+  res.status(201).json({
+    message: "Todo created successfully",
+    data: todo,
+  });
+};
