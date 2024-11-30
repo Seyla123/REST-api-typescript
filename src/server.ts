92fc822e1cbd973ef8a20d28315c3227e7b1dac3
Seav <mrrseyla.758@gmail.com>
2024-11-30 14:11:09 +0700

    feat(express) :set up basic server express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('hello 123!');
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Welcome to my API");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
