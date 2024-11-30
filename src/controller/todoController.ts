b0236ee7ce59ee581db0a285fe442117821eb4b6
Seav Seyla <mrrseyla.758@gmail.com>
2024-11-30 21:00:30 +0700

    feat(todo):add simple api todos

--- src/controller/todoController.ts
+++ src/controller/todoController.ts
@@ -1,8 +1,46 @@
 import { Request, Response } from 'express';
+
+// create todo
 export const createTodo = (req: Request, res:Response) => {
     const { title } = req.body;
     res.status(201).json({
         message: 'Todo created successfully',	
         title
+    });
+}
+
+// get all todos
+export const getAllTodos = (req: Request, res:Response) => {
+    res.status(200).json({
+        message: 'All todos fetched successfully',	
+    });
+}
+
+// update todo
+export const updateTodo = (req: Request, res:Response) => {
+    const { id } = req.params;
+    const { title, completed } = req.body;
+    res.status(200).json({
+        message: 'Todo updated successfully',	
+        title,
+        id,
+        completed
+    });
+}
+
+// delete todo
+export const deleteTodo = (req: Request, res:Response) => {
+    const { id } = req.params;
+    res.status(200).json({
+        message: 'Todo deleted successfully',	
+    });
+}
+
+// get todo by id
+export const getTodoById = (req: Request, res:Response) => {
+    const { id } = req.params;
+    res.status(200).json({
+        message: 'Todo fetched successfully',	
+        id
     });
 }
--- src/routes/todoRoute.ts
+++ src/routes/todoRoute.ts
@@ -5,6 +5,13 @@ const router = express.Router();
 
 router
     .route('/')
-    .post(todoController.createTodo);
+    .post(todoController.createTodo)
+    .get(todoController.getAllTodos);
+
+router
+    .route('/:id')
+    .get(todoController.getTodoById)
+    .patch(todoController.updateTodo)
+    .delete(todoController.deleteTodo);
 
 export default router;
