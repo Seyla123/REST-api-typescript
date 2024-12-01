a9c1e759bfdab8cda49a91e927b0c1fce9973e81
Seav Seyla <mrrseyla.758@gmail.com>
2024-12-01 16:13:30 +0700

    deploy : add vercel json

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,10 @@
 import { Sequelize } from "sequelize";
+import path from "path"; // Import path module
 
 const db = new Sequelize({
-    storage: './database.sqlite',
+    storage: path.resolve(__dirname, 'database.sqlite'), // Use an absolute path for SQLite storage
     dialect: 'sqlite',
     logging: false,
-})
+});
 
 export default db;
